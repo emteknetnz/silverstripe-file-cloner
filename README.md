8425acfc3545e8e8e48c76a5148d73ca272d06e5
Steve Boyd <emteknetnz@gmail.com>
2023-05-24 20:10:43 +1200

    .

--- README.md
+++ README.md
@@ -4,17 +4,22 @@ A quick silverstripe build task to generate lots of files
 
 Only use this for testing!
 
+**Important!** if you copy images you'll end up with 4x the number of files that you expect because variants will be created, so recommend you just use PDF files
+
 ## Instalation:
 
 `composer require emteknetnz/silverstripe-file-cloner`
 
 ## Usage:
 
-Login as admin
-
-`dev/tasks/FileClonerTask?fileID=123&folderID=456&num=1000`
+1. Login as admin
+2. Ensure there are 2x folders:
+- First folder contains sample files that will be cloned - recommend you use PDFs
+- Second folder is where you will clone the files to
+3. Ensure the files in the first folder have been published
+4. Run the build task `https://mysite.test/dev/tasks/FileClonerTask?fileID=123&folderID=456&num=1000`
 
 ### Querystring parameters
-- fileID - the file ID of the file you wish to clone
-- folderID - the folder ID of the folder you wish to clone all the files into
+- fileID - the file ID of the file you wish to clone from the first folder
+- folderID - the folder ID of the second folder you wish to clone all the files into
 - num - the number of files to clone - between 1 and 10,000
--- src/FileClonerTask.php
+++ src/FileClonerTask.php
@@ -6,17 +6,21 @@ use SilverStripe\Dev\BuildTask;
 use SilverStripe\Security\Permission;
 use SilverStripe\Assets\File;
 use SilverStripe\Assets\Folder;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Assets\Storage\AssetStore;
 
 class FileClonerTask extends BuildTask
 {
     protected $title = 'File Cloner Task';
+
     protected $description = 'Clones an existing file into an existing folder a large number of times';
-    private static $segment = 'CleanupTestDatabasesTask';
+
+    private static $segment = 'FileClonerTask';
 
     public function run($request)
     {
         if (!Permission::checkMember(null, 'ADMIN')) {
-            echo 'You must be logged in as an admin to run this task';
+            $this->log('You must be logged in as an admin to run this task');
             return;
         }
 
@@ -25,37 +29,54 @@ class FileClonerTask extends BuildTask
         $num = $request->getVar('num');
 
         if (!$folderID || !$fileID || !$num) {
-            echo 'Please provide query string values for folderID, fileID and num';
+            $this->log('Please provide query string values for folderID, fileID and num');
             return;
         }
 
         $file = File::get()->byID($fileID);
         if (!$file) {
-            echo 'File not found';
+            $this->log('File not found');
+            return;
+        }
+
+        if (!$file->isPublished()) {
+            $this->log('File must be published so that it is in the public asset store');
             return;
         }
 
         $folder = Folder::get()->byID($folderID);
         if (!$folder) {
-            echo 'Folder not found';
+            $this->log('Folder not found');
             return;
         }
 
         if ($num > 10000) {
-            echo 'Please provide a num between 1 and 10,000';
+            $this->log('Please provide a num between 1 and 10,000');
             return;
         }
 
         $fileID = $file->ID;
         $t = time();
+
+        $store = Injector::inst()->get(AssetStore::class);
+        $filesystem = $store->getPublicFilesystem();
+        $stream = $filesystem->readStream($file->Filename);
+
         for ($i = 1; $i <= $num; $i++) {
-            $filename = "$fileID-$t-$i";
-            // copy the physical-file
-            $targetPath = dirname($folder->getFileName()) . '/' . $filename;
-            echo $targetPath;
-            // $file->copyFile($targetPath);
-            // create the file DataObject
+            $newName = $folder->Filename . "$fileID-$t-$i." . $file->getExtension();
+            // will create in the protected filesystem
+            $newFile = File::create();
+            $newFile->setFromStream($stream, $newName);
+            $newFile->write();
+            // publish it to ensure that it is in the public filesystem so that if there's
+            // a mix of files they're not in different hash directories
+            $newFile->publishRecursive();
         }
-        echo "Create $num files in $folder->Name";
+        $this->log("Create $num files in $folder->Name");
+    }
+
+    private function log($s)
+    {
+        echo "$s\n";
     }
 }
